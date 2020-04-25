eb9d4e72f6b73e848fda65f1ff4342aa82f218c1
Mastermxx <62095914+Mastermxx@users.noreply.github.com>
2020-04-26 01:37:51 +0200

    Init

---
+++ js/script.js
@@ -0,0 +1,49 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance:
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/***
+ * `quotes` array
+***/
+const quotes [
+  {
+    quote: '"May the Force be with you."',
+    character : 'Han Solo',
+    actor: 'Harrison Ford',
+    movie : 'Star Wars',
+    year : '1977'
+  },
+  {
+    quote: '"May the Force be with you."',
+    character : 'Han Solo',
+    actor: 'Harrison Ford',
+    movie : 'Star Wars',
+    year : '1977'
+  },
+];
+
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
