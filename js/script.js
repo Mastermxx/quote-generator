33bf4ff1d1ff4be391095848cf9d251758085045
Mastermxx <mreijgwart@gmail.com>
2020-04-26 02:17:00 +0200

    added more quotes

--- js/script.js
+++ js/script.js
@@ -19,29 +19,54 @@ const quotes [
     year : '1977'
   },
   {
-    quote: '"May the Force be with you."',
-    character : 'Han Solo',
-    actor: 'Harrison Ford',
-    movie : 'Star Wars',
-    year : '1977'
+    quote: `"You talkin' to me?"`,
+    character : 'Travis Bickle',
+    actor: 'Robert De Niro',
+    movie : 'Taxi Driver',
+    year : '1976'
+  },
+  {
+    quote: `"Mama always said life was like a box of chocolates. You never know what you're gonna get."`,
+    character : 'Forrest Gump',
+    actor: 'Tom Hanks',
+    movie : 'Forrest Gump',
+    year : '1994'
+  },
+  {
+    quote: `"Say 'hello' to my little friend!"`,
+    character : 'Tony Montana',
+    actor: 'Al Pacino',
+    movie : 'Scarface',
+    year : '1983'
+  },
+  {
+    quote: `"Say 'hello' to my little friend!"`,
+    character : 'Tony Montana',
+    actor: 'Al Pacino',
+    movie : 'Scarface',
+    year : '1983'
+  },
+  {
+    quote: `"Keep your friends close, but your enemies closer."`,
+    character : 'Michael Corleone',
+    actor: 'Al Pacino',
+    movie : 'The Godfather Part II',
+    year : '1974'
   },
 ];
 
-"You talkin' to me?" 	Travis Bickle 	Robert De Niro 	Taxi Driver 	1976
-"Mama always said life was like a box of chocolates. You never know what you're gonna get." 	Forrest Gump 	Tom Hanks 	Forrest Gump 	1994
-"Say 'hello' to my little friend!" 	Tony Montana 	Al Pacino 	Scarface 1983
- 	"Keep your friends close, but your enemies closer."[20] 	Michael Corleone 	Al Pacino 	The Godfather Part II 1974
-  "Yo, Adrian!" 	Rocky Balboa 	Sylvester Stallone 	Rocky 	1976
-   	"I'm the King of the World!"[34] 	Jack Dawson 	Leonardo DiCaprio 	Titanic 1997
-    "My precious." 	Gollum 	Andy Serkis 	The Lord of the Rings: The Two Towers  2002
-    "They may take our lives, but they'll never take our freedom!" Braveheart, 1995
-    "My name is Maximus Decimus Meridius, commander of the Armies of the North, General of the Felix Legions and loyal servant to the true emperor, Marcus Aurelius. Father to a murdered son, husband to a murdered wife. And I will have my vengeance, in this life or the next."
-Gladiator, 2000
-"Get your stinking paws off me, you damned dirty ape!" Planet of the Apes, 1968
-"Just when I thought I was out, they pull me back in."The Godfather: Part III, 1990
-"Wax on, wax off." The Karate Kid, 1984
-"I mean, funny like I'm a clown? I amuse you?" Goodfellas, 1990
-"Hasta la vista, baby." Terminator 2: Judgment Day, 1991
+// more quotes, add later
+
+// "Yo, Adrian!" 	Rocky Balboa 	Sylvester Stallone 	Rocky 	1976
+// "I'm the King of the World!"[34] 	Jack Dawson 	Leonardo DiCaprio 	Titanic 1997
+// "My precious." 	Gollum 	Andy Serkis 	The Lord of the Rings: The Two Towers  2002
+// "They may take our lives, but they'll never take our freedom!" Braveheart, 1995
+// "My name is Maximus Decimus Meridius, commander of the Armies of the North, General of the Felix Legions and loyal servant to the true emperor, Marcus Aurelius. Father to a murdered son, husband to a murdered wife. And I will have my vengeance, in this life or the next." Gladiator, 2000
+// "Get your stinking paws off me, you damned dirty ape!" Planet of the Apes, 1968
+// "Just when I thought I was out, they pull me back in."The Godfather: Part III, 1990
+// "Wax on, wax off." The Karate Kid, 1984
+// "I mean, funny like I'm a clown? I amuse you?" Goodfellas, 1990
+// "Hasta la vista, baby." Terminator 2: Judgment Day, 1991
 
 
 
