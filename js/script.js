6a8d4b74644adf476361eed9e778c6483dfc6a7a
Mastermxx <mreijgwart@gmail.com>
2020-04-26 02:09:15 +0200

    bla

--- js/script.js
+++ js/script.js
@@ -27,6 +27,31 @@ const quotes [
   },
 ];
 
+"You talkin' to me?" 	Travis Bickle 	Robert De Niro 	Taxi Driver 	1976
+"Mama always said life was like a box of chocolates. You never know what you're gonna get." 	Forrest Gump 	Tom Hanks 	Forrest Gump 	1994
+"Say 'hello' to my little friend!" 	Tony Montana 	Al Pacino 	Scarface 1983
+ 	"Keep your friends close, but your enemies closer."[20] 	Michael Corleone 	Al Pacino 	The Godfather Part II 1974
+  "Yo, Adrian!" 	Rocky Balboa 	Sylvester Stallone 	Rocky 	1976
+   	"I'm the King of the World!"[34] 	Jack Dawson 	Leonardo DiCaprio 	Titanic 1997
+    "My precious." 	Gollum 	Andy Serkis 	The Lord of the Rings: The Two Towers  2002
+    "They may take our lives, but they'll never take our freedom!" Braveheart, 1995
+    "My name is Maximus Decimus Meridius, commander of the Armies of the North, General of the Felix Legions and loyal servant to the true emperor, Marcus Aurelius. Father to a murdered son, husband to a murdered wife. And I will have my vengeance, in this life or the next."
+Gladiator, 2000
+"Get your stinking paws off me, you damned dirty ape!" Planet of the Apes, 1968
+"Just when I thought I was out, they pull me back in."The Godfather: Part III, 1990
+"Wax on, wax off." The Karate Kid, 1984
+"I mean, funny like I'm a clown? I amuse you?" Goodfellas, 1990
+"Hasta la vista, baby." Terminator 2: Judgment Day, 1991
+
+
+
+
+
+
+
+
+
+
 
 
 /***
